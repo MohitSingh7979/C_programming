bdda3010b9b95b139dbc31c525d9c5657d58333c
MohitSingh7979 <mohitsinghatwork@gmail.com>
2023-03-11 12:51:17 +0530

    testign better comment on malloc.c

--- memory/malloc.c
+++ memory/malloc.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<stdlib.h> //! required for malloc
 
 int main()
 {
-
     int i, n;
 
     printf("Enter the number of intergers: ");
     scanf("%d", &n);
     
     int *ptr = (int *)malloc(n * (sizeof(int)));
-    
+    //* variable - typecasting - returning void - scanf - bytes for allocating in void
+                                    
     if (ptr == NULL){
         printf("Memory not available");
         exit(1);
@@ -19,6 +19,7 @@ int main()
     for (i = 0; i < n; i++){
         printf("Enter an interger: ");
         scanf("%d", ptr + i);
+        // # ptr does not have & bcoz its already returns address
     }
     
     for (i = 0; i < n; i++){
