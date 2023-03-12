5ca7709e20ada5a3a0a2c9f4f7ceb3238a8c90f2
MohitSingh7979 <mohitsinghatwork@gmail.com>
2023-03-12 13:29:00 +0530

    added 3rd node

--- linked listed/singly-linked-list.c
+++ linked listed/singly-linked-list.c
@@ -9,25 +9,40 @@ struct node
 
 int main()
 {
-    struct node *head = malloc(sizeof(struct node));
-    struct node *current = malloc(sizeof(struct node));
 
     //head node
+    struct node *head = malloc(sizeof(struct node));
     head->data = 5;
     head->link = NULL;
 
-    //current node
+    //current node aka 2 node
+    struct node *current = malloc(sizeof(struct node));
     current->data = 23;
     current->link = NULL;
     head->link = current;
+
+    //current node aka 3 node
+    struct node *current2 = malloc(sizeof(struct node));
+    current2->data = 56;
+    current2->link = NULL;
+    head->link->link = current2;
     
-    printf("head address: %lu\n", head);
-    printf("head data: %d\n", head->data);
+    printf("head address: %p\n", head);
+    printf("head data aka node 1: %d\n", head->data);
     
-    printf("current address: %lu\n", head->link);
-    printf("current data : %d\n", current->data);
-    printf("current lilnk: %d\n", current->link);
+    printf("\ncurrent address: %p\n", head->link);
+    printf("current data aka node 2: %d\n", current->data);
+    printf("current link: %p\n", current->link);
     
+    printf("\ncurrent2 address: %p\n", head->link->link);
+    printf("current2 data aka node 3: %d\n", current2->data);
+    printf("current2 link: %p\n", current2->link);
     
     return 0;
 }
+/*  [5  link]->[23 link]-> [56 link] = NULL
+     ^          ^           ^
+     |          |           |
+
+ADD head       current     current2
+*/
